2333ae88c0a2103107371e207190f3ed7fbb296b
Olivia Mo <oliviatrademark@gmail.com>
2017-09-09 22:01:24 +1000

    update README.md, tried to fix smooth scrolling

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  get 'welcome/sitemap', to: 'welcome#sitemap'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
