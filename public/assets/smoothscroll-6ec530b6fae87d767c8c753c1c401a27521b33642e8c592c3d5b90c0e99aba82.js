62100ae7dd613d7c5885c533f968cce5cea13329
Olivia Mo <oliviatrademark@gmail.com>
2017-09-09 20:14:55 +1000

    added smooth scrolling, updated README.md

---
+++ public/assets/smoothscroll-6ec530b6fae87d767c8c753c1c401a27521b33642e8c592c3d5b90c0e99aba82.js
@@ -0,0 +1,44 @@
+(function (root, factory) {
+	if ( typeof define === 'function' && define.amd ) {
+		define([], function () {
+			return factory(root);
+		});
+	} else if ( typeof exports === 'object' ) {
+		module.exports = factory(root);
+	} else {
+		root.SmoothScroll = factory(root);
+	}
+})(typeof global !== 'undefined' ? global : typeof window !== 'undefined' ? window : this, function (window) {
+
+	'use strict';
+
+	//
+	// Feature Test
+	//
+
+	var supports =
+		'querySelector' in document &&
+		'addEventListener' in window &&
+		'requestAnimationFrame' in window &&
+		'closest' in window.Element.prototype;
+
+
+	//
+	// Default settings
+	//
+
+	var defaults = {
+		// Selectors
+		ignore: '[data-scroll-ignore]',
+		header: null,
+
+		// Speed & Easing
+		speed: 500,
+		offset: 0,
+		easing: 'easeInOutCubic',
+		customEasing: null,
+
+		// Callback API
+		before: function () {},
+		after: function () {}
+	};
